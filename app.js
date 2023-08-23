3b89363d6a6be2d88b1f37d94fae9a3989cb3d17
Mohamed Shareef <m.shareef.km@gmail.com>
2023-08-23 20:20:18 +0530

    some packages added for user registration

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-//imports
-
+//imports packages
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
+import morgan from "morgan"
+
+//imports files
 import connectDB from "./config/db.js"
+import userRouter from "./routes/user.js"
 
 //Dotenv config
 dotenv.config()
@@ -10,13 +14,14 @@ dotenv.config()
 //mongodb connection
 connectDB()
 
-
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.send("<h1>welcome to yummi<h1>")
-    
-})
+//middlewares
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+app.use('/api/auth',userRouter)
 //port
 const PORT = process.env.PORT
 
