d26278ffe630f5536c6af8bc240c2c2d591d33e8
Mohamed Shareef <m.shareef.km@gmail.com>
2023-08-30 21:00:43 +0530

    userLogin some changes

--- config/bcript.js
+++ config/bcript.js
@@ -5,5 +5,9 @@ export const genPass = {
         const salt = await bcrypt.genSalt(10)
         const newPass = await bcrypt.hash(pass,salt)
         return newPass
+    },
+    compairePass:async function(bodyPass,pass){
+        const isMatch = await bcrypt.compare(bodyPass,pass)
+        return isMatch
     }
 }
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,5 +4,25 @@ export const auth = {
     genrateToken:(id)=>{
         const token = jwt.sign({id:id},process.env.JWT_SECRET,{expiresIn:'1d'})
         return token;
+    },
+    verifyToken:async (req, res, next) => {
+        let token = req.header("Authorization");
+        console.log(token);
+        try {
+            if (!token) return res.status(404).json({ message: "Authentication failed: no token provided." });
+    
+            if (token.startsWith("Bearer ")) {
+                
+                token = token.slice(7, token.length).trimLeft();
+                
+            }
+            const verified = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = verified;
+    
+            next();
+        } catch (error) {
+            console.log(error);
+            return res.status(404).json({ message: "Authentication failed: invalid token." });
+        }
     }
 }
