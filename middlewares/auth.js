38e26e956aa061761ea67e4604ac6d40b9b594ff
Mohamed Shareef <m.shareef.km@gmail.com>
2023-09-21 09:19:40 +0530

    Restaurant auth added

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,28 +1,71 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 export const auth = {
-    genrateToken:(id)=>{
-        const token = jwt.sign({id:id},process.env.JWT_SECRET,{expiresIn:'1d'})
-        return token;
-    },
-    verifyToken:async (req, res, next) => {
-        let token = req.header("Authorization");
-        console.log(token);
-        try {
-            if (!token) return res.status(404).json({ message: "Authentication failed: no token provided." });
-    
-            if (token.startsWith("Bearer ")) {
-                
-                token = token.slice(7, token.length).trimLeft();
-                
-            }
-            const verified = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = verified;
-    
-            next();
-        } catch (error) {
-            console.log(error);
-            return res.status(404).json({ message: "Authentication failed: invalid token." });
-        }
+  genrateToken: (id) => {
+    const token = jwt.sign({ id: id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    return token;
+  },
+  
+  verifyToken: async (req, res, next) => {
+    let token = req.headers.authorization;
+    try {
+      if (!token || !token.startsWith("Bearer "))
+        return res
+          .status(404)
+          .json({ message: "Authentication failed: Please Login..." });
+      token = token.split(" ")[1];
+      const verified = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = { userId: verified.id };
+      next();
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(404)
+        .json({ message: "Authentication failed: invalid Login..." });
     }
-}
+  },
+
+  adminVerify: async (req, res, next) => {
+    let token = await req.headers.authorization;
+    try {
+      if (!token || !token.startsWith("Bearer "))
+        return res
+          .status(404)
+          .json({ message: "Authentication failed: Please Login..." });
+
+      token = token.split(" ")[1];
+
+      const verified = jwt.verify(token, process.env.JWT_SECRET);
+      req.admin = { adminId: verified.id };
+      next();
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(404)
+        .json({ message: "Authentication failed: invalid Login..." });
+    }
+  },
+
+  restaurantVerify: async (req, res, next) => {
+    let token = await req.headers.authorization;
+    try {
+      if (!token || !token.startsWith("Bearer "))
+        return res
+          .status(404)
+          .json({ message: "Authentication failed: Please Login..." });
+
+      token = token.split(" ")[1];
+
+      const verified = jwt.verify(token, process.env.JWT_SECRET);
+      req.restaurant = { restaurantId: verified.id };
+      next();
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(404)
+        .json({ message: "Authentication failed: invalid Login..." });
+    }
+  },
+};
